b63d3df96b68e675ebcb0810bb34c8391333a64f
AlexeyKhodasevich <powerbandtestalex@gmail.com>
2023-04-13 21:35:57 +0300

    Assert class removed

--- src/test/java/unittest/ReturnNameLatNameTest.java
+++ src/test/java/unittest/ReturnNameLatNameTest.java
@@ -1,28 +1,29 @@
 package unittest;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class ReturnNameLatNameTest {
     ReturnNameLastName returnNameLastName = new ReturnNameLastName();
 
     @Test
     public void numbersMultiplied35Test() {
-        Assert.assertEquals(returnNameLastName.getNameLastname(15), "Alex Khadasevich", "Method didn't return Full Name");
+        assertEquals(returnNameLastName.getNameLastname(15), "Alex Khadasevich", "Method didn't return Full Name");
     }
 
     @Test
     public void numbersMultiply3Test() {
-        Assert.assertEquals(returnNameLastName.getNameLastname(9), "Alex", "Method didn't return First Name");
+        assertEquals(returnNameLastName.getNameLastname(9), "Alex", "Method didn't return First Name");
     }
 
     @Test
     public void numbersMultiply5Test() {
-        Assert.assertEquals(returnNameLastName.getNameLastname(20), "Khadasevich", "Method didn't return Last name");
+        assertEquals(returnNameLastName.getNameLastname(20), "Khadasevich", "Method didn't return Last name");
     }
 
     @Test
     public void numbersNotMultiplied35Test() {
-        Assert.assertEquals(returnNameLastName.getNameLastname(11), "Ошибка", "Method didn't return error message");
+        assertEquals(returnNameLastName.getNameLastname(11), "Ошибка", "Method didn't return error message");
     }
 }
